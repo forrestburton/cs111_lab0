c692d4b3c7a0a98b5caa1af8fb0f7bd09314fe78
Forrest Burton <burton.forrest10@gmail.com>
2021-01-11 17:58:21 -0800

    options processing complete

--- lab0.c
+++ lab0.c
@@ -5,6 +5,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
 
 void cause_segfault() {
     char* ptr = NULL;
@@ -12,10 +13,15 @@ void cause_segfault() {
     return;
 }
 
+void handle_sigint(int sig) {
+    fprintf(stderr, "Segmentation fault caught");
+    exit(4);
+}
+
 int main(int argc, char *argv[]) {
-    int arg;
-    char* input, output;
-    bool seg_fault = false;
+    char* input = NULL;
+    char* output = NULL;
+    bool seg_fault = 0;
 
     while(1) {
         int option_index = 0;
@@ -25,27 +31,59 @@ int main(int argc, char *argv[]) {
             {"segfault", no_argument, 0, 's' },
             {"catch", no_argument, 0, 'c' },
             {0,     0,                  0, 0 }};
-        c = getopt_long(argc, argv, "ab:0", longoptions, &option_index);
+        c = getopt_long(argc, argv, "", long_options, &option_index);
         if (c == -1) break;
         switch (c) {
             case 'i':
-                //int file = open(optarg, 0);
+                input = optarg;
                 break;
             case 'o':
-                printf("option a\n");
+                output = optarg;
                 break;
             case 's':
-                printf("option b with value '%s'\n", optarg);
-                seg_fault = true;
+                seg_fault = 1;
                 break;
             case 'c':
+                signal(SIGSEGV, handle_sigint);
                 break;
             default:
-                printf("?? getopt returned character code 0%o ??\n", c);
+                printf("unrecognized argument %o ??\n", c);
+                exit(1);
+        }
+    }
+
+    if (input) {
+        int ifd = open(input, O_RDONLY);
+        if (ifd >= 0) {
+            close(0);
+            dup(ifd);
+            close(ifd);
+        }
+        else {
+            fprintf(stderr, "--input error while opening the file: %s , which was caused by: %s\n", input, strerror(errno));
+            exit(2);
+        }
+    }
+
+    if (output) {
+        int ofd = open(output, O_RDONLY);
+        if (ofd >= 0) {
+            close(1);
+            dup(ofd);
+            close(ofd);
+        }
+        else {
+            fprintf(stderr, "--output error while opening the file: %s , which was caused by: %s\n", output, strerror(errno));
+            exit(3);
         }
     }
 
     if (seg_fault) {
         cause_segfault();
     }
+
+    //copies standard input to standard output by read(2)-ing from file descriptor 0 (until encountering an end of file) and write(2)-ing to file descriptor 1
+    
+
+    exit(0);
 }
